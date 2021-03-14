3d90d9dc30dc656a9fe3dbefc6a0386479c65548
woocyouth <15107510579@163.com>
2021-03-14 16:25:37 +0800

    code2-02

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(function () {
+    getUserInfo()
+})
+
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem("my2Token") || ""
+        },
+        success: (res) => {
+            // console.log(res);
+            if (res.status != 0) {
+                layer.msg(res.message, {
+                    icon: 5
+                });
+                location.href = "/login.html";
+                return;
+            }
+
+            render(res.data);
+        }
+    })
+}
+
+function render(data) {
+    // console.log(data); 
+    let name = data.nickname || data.username;
+    $(".welcome").text("欢迎  " + name);
+    if (data.user_pic) {
+        $(".layui-nav-img").attr("src", data.user_pic);
+        $(".text-avatar").hide();
+        $(".username-top").text(name);
+    } else {
+        $(".layui-nav-img").hide();
+        $(".text-avatar").text(name[0].toUpperCase());
+        $(".username-top").text(name);
+    }
+}
